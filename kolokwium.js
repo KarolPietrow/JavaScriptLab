a6800f260f38350bf5f1054d7d258f45613d44ee
Karol Pietrów <karol.pietrow@icloud.com>
2025-06-20 22:15:44 +0200

    Add ex.4

--- kolokwium.js
+++ kolokwium.js
@@ -71,3 +71,20 @@ const staraKsiega = [
     "legendarna ochrona 30"
 ]
 console.log(wybierzZaklecia(staraKsiega));
+
+// 4
+function sum(arr, target) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                result.push([arr[i], arr[j]]);
+            }
+        }
+    }
+    return result;
+}
+
+nums2 = [1, 4, 3, 2, 5, 7]
+target = 6
+console.log(sum(nums2, target))
